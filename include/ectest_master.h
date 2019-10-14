5bb4533e2c0e9f5c00fde8fb8e76f28ff2cfe7a0
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2019-10-14 13:38:00 +0200

    reordered includes

--- include/ectest_master.h
+++ include/ectest_master.h
@@ -5,11 +5,11 @@
 #include <string.h> //ECT_NO_MERGE
 #include <stdio.h> //ECT_NO_MERGE
 #include <ectest_api.h>
+#include <ectest_utils.h>
 #include <ectest_types.h>
 #include <ectest_alloc.h>
 #include <ectest_expimp.h>
 #include <ectest_suit.h>
-#include <ectest_utils.h>
 
 #endif /* ECTEST_MASTER__ */
 
