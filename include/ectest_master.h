07ad7642a1f02c36a7a7de4c28bdaebba57e7f0c
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2019-10-17 21:02:09 +0200

    added setjmp.h inclusion

--- include/ectest_master.h
+++ include/ectest_master.h
@@ -4,6 +4,7 @@
 #include <stdlib.h> //ECT_NO_MERGE
 #include <string.h> //ECT_NO_MERGE
 #include <stdio.h> //ECT_NO_MERGE
+#include <setjmp.h> //ECT_NO_MERGE
 #include <ectest_api.h>
 #include <ectest_utils.h>
 #include <ectest_types.h>
@@ -12,4 +13,3 @@
 #include <ectest_suit.h>
 
 #endif /* ECTEST_MASTER__ */
-
