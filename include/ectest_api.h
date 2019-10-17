0eabece5699341e2ec2b6b405ee5ae8b7ce6ab91
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2019-10-17 21:01:00 +0200

    Removed #define's from api.h, and simply added comments for API functions/macros

--- include/ectest_api.h
+++ include/ectest_api.h
@@ -1,39 +1,35 @@
 #ifndef ECTEST_API__
 #define ECTEST_API__
 
-#define ECT_EXPORT_MODULE(module, functions...)                                 \
-        ECT_EXPORT_MODULE__(module, functions)
-    
-#define ECT_TEST(func)                                                          \
-        ECT_TEST__(func)
-
-#define ECT_TEST_MODSETUP(func)                                                 \
-        ECT_TEST_MODSETUP__(func)
-
-#define ECT_TEST_MODTEARDOWN(func)                                              \
-        ECT_TEST_MODTEARDOWN__(func)
-
-#define ECT_TEST_SETUP(func)                                                    \
-        ECT_TEST_SETUP__(func)
-
-#define ECT_TEST_TEARDOWN(func)                                                 \
-        ECT_TEST_TEARDOWN__(func)
-    
-#define ECT_IMPORT_MODULE(module)                                               \
-        ECT_IMPORT_MODULE__(module)
-        
-#define ECT_SUITE_NEW(suitname, modules...)                                      \
-        ECT_SUITE_NEW__(suitname, modules)
-
-#define ECT_SUITE_FREE(suit)                                                     \
-        ect_suite_free(suit)         
-        
-        
-#define ECT_SIZEOF_ARRAY(array)                                                 \
-        ECT_SIZEOF_ARRAY__(array)
-
-#define ECT_ARRAY_FOREACH(item, array, size)                                    \
-        ECT_ARRAY_FOREACH__(item, array, size)   
+/*
+ * CPPTOKEN: 
+ *      Marks where a C Preprocessor valid token should be passed as an
+ *      an argument to a macro.
+ * 
+ * TYPE: 
+ *      Marks where a generic type can be passed as an argument
+ *      to a macro.
+ *
+ * ect_export_module(CPPTOKEN module, void (*functions)(void)...)
+ * 
+ * ect_test(void (*function)(void))
+ * 
+ * ect_module_setup(void (*function)(void))
+ * 
+ * ect_module_teardown(void (*function)(void))
+ * 
+ * ect_test_setup(void (*function)(void))
+ * 
+ * ect_test_teardown(void (*function)(void))
+ * 
+ * ect_import_module(void (*module)(void))
+ * 
+ * ect_suite_new(CPPTOKEN suitename, void (*modules)(void)...)
+ * 
+ * ect_suite_free(ect_suite *suite)
+ * 
+ * ECT_SIZEOF_ARRAY(TYPE array[])
+ */  
 
 #endif /*ECTEST_API__*/
 
