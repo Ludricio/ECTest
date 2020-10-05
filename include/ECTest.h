74d305df19c1609d08ccd71e5f324d3a9d98ef97
Ludvig Rydahl <ludvig.rydahl@gmail.com>
2020-10-05 16:18:48 +0200

    added __ECTEST_TYPES_H_ header guard

--- include/ECTest.h
+++ include/ECTest.h
@@ -3,6 +3,10 @@
 #ifndef __ECTEST_MASTER_H_
 #define __ECTEST_MASTER_H_
 
+
+#ifndef __ECTEST_TYPES_H_
+#define __ECTEST_TYPES_H_
+
 typedef void (*__ectest_tfn)(void);
 
 struct __ectest_test{
@@ -20,6 +24,9 @@ struct __ectest_suite{
     int nmodules;
     struct __ectest_module modules[];
 };
+#endif /*__ECTEST_TYPES_H_*/
+
+
 
 
 #endif /*__ECTEST_MASTER_H_*/
